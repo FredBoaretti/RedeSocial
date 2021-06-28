7f963e8c62bcda2ed34af863ec1492cd2ced36c3
FredBoaretti <frederico.boaretti@gmail.com>
2021-06-27 21:30:59 -0300

    Feat: Trabalho OK e Finalizado

---
+++ app/src/main/java/com/example/redesocial/ItemChat.kt
@@ -0,0 +1,10 @@
+package com.example.redesocial
+
+data class ItemChat(
+    val photo: String,
+    var username: String,
+    var age: Double,
+    var gender: String,
+    var sexualOrientation: String,
+    val city: String
+)
